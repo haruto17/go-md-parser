8ae6032a2487454bbe996296b8f83e4fb278b81a
haruto17 <proaca157@gmail.com>
2026-04-29 13:24:05 +0900

    ✨️ １行毎にヘッダー及び段落をHTMLに変換する処理を実装

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,5 +1,26 @@
 package parser
 
+import (
+	"strings"
+)
+
 func Parse(input string) string {
-	return input
+	lines := strings.Split(input, "\n")
+	var out []string
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		if content, ok := strings.CutPrefix(line, "# "); ok {
+			out = append(out, "<h1>"+content+"</h1>")
+			continue
+		}
+
+		out = append(out, "<p>"+line+"</p>")
+	}
+
+	return strings.Join(out, "\n")
 }
